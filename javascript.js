aa7c04927b3391e19492f1a090d19f5bf812e609
Dustin <dustin@sent.com>
2023-06-04 12:10:33 -0400

    first commit, created divs with IDs

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function plus(a, b){
+    return a + b
+}
+
+function minus(a, b){
+    return a - b
+}
+
+function multiply (a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
